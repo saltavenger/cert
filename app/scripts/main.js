af8efea1e243cd3bcfd83cde867751316cda68c3
areskoga <ali_areskog@abtassoc.com>
2015-07-22 21:46:50 -0400

    lots of changes to dashboard charts and addition of a second submenu to main nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,21 +20,93 @@
       }
     },
     requestEnd: function(objData){
-      buildColumnChart('dash1', objData.response, false, 'docketData', 'Comments in Docket vs Comments in CommentCounts', 'value', 'metric', 'cat');
-      buildColumnChart('dash2', objData.response, false, 'commentData', 'Total Comment Excerpts vs Total Responses', 'value', 'metric', 'cat');
-      buildColumnChart('dash3', objData.response, false, 'responseData', 'Draft Responses vs Final Responses', 'value', 'metric', 'cat');
-      buildColumnChart('dash4', objData.response, false, 'orgNumData', 'Total Number of Organizations Submitting Comments', 'value', 'cat');
-      buildColumnChart('dash5', objData.response, true, 'orgTypeData', 'Comments by Organization Type', 'count', 'orgtype', 'group');
-      buildColumnChart('dash6', objData.response, false, 'headerData', 'Top Five Outline Headings (Comment Excerpts)', 'number_in_outline', 'outline_number', 'cat');
+      var chartColors = ['#a6cee3','#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928', '#000000', '#2550A7'],
+      dash1Options = {
+        series: [{
+          field: "value",
+          categoryField: "metric",
+          type: "pie",
+          labels: {
+            visible: true,
+            template: "#var percent= percentage;# # percent= kendo.toString(percent, \"p0\"); # #= percent#",
+          }
+        }],
+        legend: {
+          visible: true
+        },
+        tooltip:{
+          visible: true,
+          template: "#= category#: #= value#"
+        }
+      },
+      dash2Options = {
+        series: [{
+          field: "value",
+          categoryField: "group",
+          stack: true
+        }],
+        legend: {
+          visible: true
+        },
+        tooltip:{
+          visible: true,
+          template: "#= dataItem.metric#: #= value#"
+        }
+      },
+      dash3Options = {
+        series: [{
+          type: "horizontalWaterfall",
+          field: "count",
+          categoryField: "orgtype",
+          summaryField: "summary",
+          color: function(point){
+            return chartColors[point.index];
+          }
+        }],
+        legend: {
+          visible: false
+        },
+        categoryAxis:{
+        },
+        tooltip:{
+          visible: true,
+          template: "<b>#= dataItem.orgtype#</b><br/>#= value# Comments"
+        }
+      },
+      dash4Options ={
+        series: [{
+          field: "number_in_outline",
+          categoryField: "outline_number",
+          stack: true
+        }],
+        legend: {
+          visible: true
+        },
+        categoryAxis:{
+          labels:{
+            visible: false
+          }
+        },
+        tooltip:{
+          visible: true,
+          template: "<div style='text-align: left;'><b>Description:</b> #= dataItem.outline_description#<br/><b>Comments:</b> #= value#</span>",
+          align: "left"
+        }
+      };
+      
+      buildChart('dash1', objData.response, 'docketData', dash1Options, chartColors, 'Comments in Docket vs Comments in CommentCounts');
+      buildChart('dash2', objData.response, 'commentData', dash2Options, chartColors, 'Total Comment Excerpts vs Total Responses', 'metric');
+      buildChart('dash3', objData.response, 'orgTypeData', dash3Options, chartColors, 'Comments by Organization Type');
+      buildChart('dash4', objData.response, 'headerData', dash4Options, chartColors, 'Top Five Outline Headings (Comment Excerpts)', 'outline_number');
     }
   });
 
   getData.read();
 
 
-  function buildColumnChart(id, data, legendViz, dataSchema, title, seriesField, groupOrcatField, groupOrCat){
-    var chartColors = ['#a6cee3','#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928', '#000000', '#2550A7'],
-    kendoOptions = {
+  function buildChart(id, data, dataSchema, chartOptions, chartColors, title, groupField){
+    var baseOptions = {
+      theme: "material",
       dataSource: {
         data: data,
         schema: {
@@ -46,35 +118,14 @@
       title: {
         text: title
       },
-      seriesColors: chartColors,
-      series: [{
-        field: seriesField
-      }],
-      legend: {
-        visible: legendViz,
-        position: 'right'
-      },    
-      tooltip: {
-        visible: true
-      }
-    };
-    if(typeof groupOrcatField !== 'undefined'){
-      if(groupOrCat === 'group'){
-        kendoOptions.dataSource.group = { field: groupOrcatField};
-        kendoOptions.series[0].name = '#: group.value #';
-        kendoOptions.categoryAxis = { labels: { visible: false} };
-        kendoOptions.title.align = "left";
-      }
-      else{
-        kendoOptions.categoryAxis = { field: groupOrcatField };
-        kendoOptions.categoryAxis.labels = { template: '# var label= value;# #label = label.substring(0,25);# #= label #' };
-        kendoOptions.series[0].color = function(point){
-          var seriesColor = chartColors;
-          return seriesColor[point.index];
-        };
-      }
+      seriesColors: chartColors
+    },
+    kendoOptions = $.extend(true, baseOptions, chartOptions);
+    if(typeof groupField !== 'undefined'){
+      kendoOptions.dataSource.group = { field: groupField};
     }
     $('#' + id).kendoChart(kendoOptions);
   }
 
 })();
+
