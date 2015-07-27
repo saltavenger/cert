ed7fc5bd142a70a9204b012fe101be714134456f
areskoga <ali_areskog@abtassoc.com>
2015-07-27 14:31:48 -0400

    changes for hover/focus of submenus

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function(){
-  $('.dropdown a').on('mouseenter focus', function(){
+  $('li.dropdown, li.dropdown a').on('mouseenter focus', function(){
     $(this).closest('.dropdown').addClass('active');
   }).on('mouseleave blur', function(){
     $(this).closest('.dropdown').removeClass('active');
